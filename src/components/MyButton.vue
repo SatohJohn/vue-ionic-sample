1a453587a024ad4f2e2432ca52f57752268fbadc
john <sato.keita@furyu.jp>
2018-12-20 19:29:56 +0900

    inputが動かない問題を修正

--- src/components/MyButton.vue
+++ src/components/MyButton.vue
@@ -1,24 +1,32 @@
 <template>
   <div>
     <ion-button shape="round" color="primary" fill="outline" @click="showModal">Show Modal</ion-button>
-    <ion-input required type="text" placeholder="First Name"></ion-input>
+    <ion-input required type="text" @ionChange="change" placeholder="First Name"></ion-input>
   </div>
 </template>
 
 <script>
 export default {
   name: 'MyButton',
+  data() {
+      return {
+          message: ''
+      }
+  },
   methods: {
     async showModal() {
       // ボタンがクリックされたらionicのモーダルを表示
       const loading = await this.$ionic.loadingController.create({
         spinner: 'show',
-        duration: 5000,
-        message: 'Please wait...',
+        duration: 3000,
+        message: this.message,
         translucent: true,
         cssClass: 'custom-class custom-loading'
       });
       loading.present();
+    },
+    change(e) {
+      this.message = e.detail.value;
     }
   }
 }
--- src/components/MyTab.vue
+++ src/components/MyTab.vue
@@ -1,6 +1,6 @@
 <template>
   <ion-tabs>
-    <ion-tab label="Music" icon="musical-note" @click="showModal"></ion-tab>
+    <ion-tab label="Music" icon="musical-note"></ion-tab>
     <ion-tab label="Profile" icon="person"></ion-tab>
     <ion-tab label="people" icon="people"></ion-tab>
   </ion-tabs>
@@ -8,25 +8,6 @@
 
 <script>
 export default {
-  name: 'MyTab',
-  methods: {
-    async showModal() {
-      const loading = await this.$ionic.loadingController.create({
-        spinner: 'show',
-        duration: 5000,
-        message: 'Please wait...',
-        translucent: true,
-        cssClass: 'custom-class custom-loading'
-      });
-      loading.present();
-    },
-    async showToast() {
-      const toast = await this.$ionic.toastController.create({
-      message: 'Your settings have been saved.',
-      duration: 2000
-      });
-      toast.present();
-    }
-  }
+  name: 'MyTab'
 }
 </script>
